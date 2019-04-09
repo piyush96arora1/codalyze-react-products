d864d7d34696a8ff7e9520bde4eebdf69f5a034c
unknown <piyush96arora@gmail.com>
2019-04-10 01:16:53 +0530

    modifying package.json

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import ProductsProvider from './Components/ProductsProvider'
-import {createStore} from 'redux';
-import {BrowserRouter, Route} from 'react-router-dom'
-import {Provider} from 'react-redux'
-import {rootReducer} from './Store/reducer'
+import { createStore } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { rootReducer } from './Store/reducer'
 import EditProduct from './Components/EditProduct'
 
-const store=createStore(rootReducer);
-const Routing=({store})=>(
-<Provider store={store}>
-<BrowserRouter>
-<Route exact path="/" component={ProductsProvider}/>
-<Route path="/edit-product" component={EditProduct}/>
- </BrowserRouter>
- </Provider>)
-ReactDOM.render(<Routing store={store}/>, document.getElementById('root'));
+const store = createStore(rootReducer);
+const Routing = ({ store }) => (
+    <Provider store={store}>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <Route exact path="/" component={ProductsProvider} />
+            <Route path="/edit-product" component={EditProduct} />
+        </BrowserRouter>
+    </Provider>)
+ReactDOM.render(<Routing store={store} />, document.getElementById('root'));
 
