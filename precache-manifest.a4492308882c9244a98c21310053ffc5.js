7a3924942bde9fab182444d1a5795696778d9d40
unknown <piyush96arora@gmail.com>
2019-04-10 00:50:51 +0530

    Updates

---
+++ precache-manifest.a4492308882c9244a98c21310053ffc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "190be77a5e625f9ba70a",
+    "url": "/codalyze-react-products/static/js/runtime~main.dbd3247f.js"
+  },
+  {
+    "revision": "df970b06160d853c0cd5",
+    "url": "/codalyze-react-products/static/js/main.14511eea.chunk.js"
+  },
+  {
+    "revision": "0eacb91992a4697a8ac1",
+    "url": "/codalyze-react-products/static/js/2.102c4071.chunk.js"
+  },
+  {
+    "revision": "df970b06160d853c0cd5",
+    "url": "/codalyze-react-products/static/css/main.45206102.chunk.css"
+  },
+  {
+    "revision": "6aae7af5cf5a459df574671dcf5e3442",
+    "url": "/codalyze-react-products/index.html"
+  }
+];
